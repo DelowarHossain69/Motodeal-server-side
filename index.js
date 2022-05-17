bb36e578cce4167eaf99584be31352049fc9b85f
DelowarHossain69 <daloar2400@gmail.com>
2022-05-17 00:21:44 -0700

    cors updated

--- index.js
+++ index.js
@@ -9,7 +9,19 @@ require("dotenv").config();
 
 // middleware
 app.use(express.json());
-app.use(cors());
+const corsConfig = {
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.options("*", cors(corsConfig))
+app.use(express.json())
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,authorization")
+  next()
+})
 
 // JWT verify
 function jwtVerify(req, res, next) {
